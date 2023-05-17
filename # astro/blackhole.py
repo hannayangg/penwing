f7e440d8e9811f5a58771284c912eda27aa90e72
yhna3940 <yhna3940@gmail.com>
2023-05-17 20:42:26 +0900

    ㅜㅜㅡ

--- # astro/blackhole.py
+++ # astro/blackhole.py
@@ -30,3 +30,48 @@ def angular_dist(r1, d1, r2, d2):
   d = 2 * np.arcsin(np.sqrt(a+b))
   # 다시 라디안을 각도로
   return np.degrees(d)
+
+
+
+
+
+
+
+
+# 데이터 불러와서 좌표 계산하기
+
+def import_bss():
+  data = np.loadtxt('bss.dat', usecols=range(1,7))
+  res = []
+  for i, row in enumerate(data):
+    hms = hms2dec(row[0], row[1], row[2])
+    dms = dms2dec(row[3], row[4], row[5])
+    res.append((i+1, hms, dms))
+  return res
+
+def import_super():
+  data = np.loadtxt('super.csv', delimiter=',', skiprows=1, usecols=[0, 1])
+  res = []
+  for i, row in enumerate(data):
+    res.append((i+1, row[0], row[1]))
+  return res
+
+
+
+
+
+
+
+
+# 가까운 물체 찾기
+
+import sys
+def find_closest(cat, r, d):
+  res_id = None
+  res_dist = np.inf
+  for row in cat:
+    dist = angular_dist(row[1], row[2], r, d)
+    if dist < res_dist:
+      res_dist = dist
+      res_id = row[0]
+  return (res_id, res_dist)
