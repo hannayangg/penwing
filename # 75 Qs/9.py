459c6ffcca4c059b08d2a62c1f95cc46e9b85da6
yhna3940 <yhna3940@gmail.com>
2023-05-21 15:30:56 +0900

    수정

--- # 75 Qs/9.py
+++ # 75 Qs/9.py
@@ -1,42 +1,24 @@
 # [9. leetcode 15]
 
-
-# 내가 푼 것: r 중복만 스킵.
-# l,r 모두 중복 스킵하는 방법 모름.
-
-
-
-
 def F(nums):
   nums.sort()
   res = []
-
-  for a in range(len(nums)-1):
-    # 양수/중복스킵
-    if (nums[a] > 0) or (a > 0 and nums[a] == nums[a-1]):
+  for i in range(len(nums)):
+    # 첫 숫자 중복 방지 [0,0,0,0]
+    if i > 0 and nums[i-1] == nums[i]:
       continue
     
-    b, c = a+1, len(nums)-1
-    while b < c:
+    l, r = i+1, len(nums)-1  
+    while l < r:
+      curSum = nums[i] + nums[l] + nums[r]
+      if curSum < 0: l += 1
+      elif curSum > 0: r -= 1
 
-      # c 중복스킵
-      if c < len(nums)-1 and nums[c] == nums[c+1]:
-        c -= 1
-        continue
-
-      # b 중복스킵 (*b가 출발시점에 있을 때는 스킵제외)
-      if b > a+1 and nums[b] == nums[b-1]:
-        b += 1
-        continue
-
-      # 투포인터
-      curSum = nums[a]+nums[b]+nums[c]
-      if curSum == 0:
-        res.append([nums[a], nums[b], nums[c]])
-        b += 1
-        c -= 1
-      elif curSum < 0:
-        b += 1
       else:
-        c -= 1
+        res.append([nums[i], nums[l], nums[r]])
+        l += 1
+        # l 중복 방지
+        while nums[l-1] == nums[l] and l < r:
+          l += 1
+  
   return res
