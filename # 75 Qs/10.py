1e1d2b19687f661aa99d89f000c04f90ef28c3c7
yhna3940 <yhna3940@gmail.com>
2023-05-22 20:10:51 +0900

    수정

--- # 75 Qs/10.py
+++ # 75 Qs/10.py
@@ -2,27 +2,15 @@
 
 
 # 투포인터.
-
 def F(nums):
   l, r = 0, len(nums)-1
   res = 0
   while l < r:
-    res = max(res, (r-l)*(min(nums[l], nums[r])))
-    if nums[l] <= nums[r]:
+    a = r-l
+    b = min(nums[l], nums[r])
+    res = max(res, a*b)
+    if nums[l] < nums[r]:
       l += 1
     else:
-      r -= 1
-  return res
-
-
-def F(nums):
-  l, r = 0, len(nums)-1
-  res = 0
-  while l < r:
-    if nums[l] <= nums[r]:
-      res = max(res, (r-l)*nums[l])
-      l += 1
-    else:
-      res = max(res, (r-l)*nums[r])
       r -= 1
   return res
