3543d78ddacee657fd6bc62b6088907617617f26
yhna3940 <yhna3940@gmail.com>
2023-06-01 20:29:15 +0900

    수정

--- # 75 Qs/17.py
+++ # 75 Qs/17.py
@@ -2,17 +2,14 @@
 
 
 # Bottom up
+# dp[i] = max(dp[i], 1 + dp[j])
+
 
 def F(nums):
   dp = [1] * len(nums)
+
   for i in range(1, len(nums)):
-    prev = []
     for j in range(i):
       if nums[j] < nums[i]:
-        prev.append(dp[j])
-    dp[i] = max(prev)+1 if prev else 1
+        dp[i] = max(dp[i], 1 + dp[j])
   return max(dp)
-
-
-# prev없는 방법: 시간초과. (왜지?)
-# dp[i] = max(dp[i], dp[j]+1)
