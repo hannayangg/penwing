5e35bf882a4a979c9c36b13b2359ef6cf0b4a542
yhna3940 <yhna3940@gmail.com>
2023-06-10 21:10:00 +0900

    수정

--- # 75 Qs/21.py
+++ # 75 Qs/21.py
@@ -1,9 +1,11 @@
 # [21. leetcode 198]
 
+
+# 1단계
 def F(nums):
   for i in range(2, len(nums)):
     if i == 2:
-      nums[i] += nums[0]
+      nums[2]+=nums[0]
       continue
     nums[i] += max(nums[i-2], nums[i-3])
   return max(nums)
@@ -11,14 +13,28 @@ def F(nums):
 
 
 
-# nums 변화 없는 풀이.
-# rob1과 rob2변수에 값들을 저장.
+
+
+# 2단계: 현재값 = max(전값, 현재값+전전값)
+def F(nums):
+    if len(nums)==1: return nums[0]
+    nums[1] = max(nums[0], nums[1])
+    for i in range(2, len(nums)):
+        nums[i] = max(nums[i-1], nums[i]+nums[i-2])
+    return nums[-1]
+
+
+
+
+
+# 3단계: 전값, 전전값을 변수로.
+# [rob1, rob2, n, ...]
+
 
 def F(nums):
   rob1, rob2 = 0, 0
   for n in nums:
     tmp = max(rob1+n, rob2)
-    # rob1, rob2 업데이트!!
     rob1 = rob2
     rob2 = tmp
   return rob2
