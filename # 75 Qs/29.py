be2ec65ab34acc1e79413cb1275bf2f582af609a
yhna3940 <yhna3940@gmail.com>
2023-06-21 18:59:35 +0900

    수정

--- # 75 Qs/29.py
+++ # 75 Qs/29.py
@@ -1,14 +1,25 @@
-# [29. leetcode 128]
+# [29. leetcode 200]
 
 
-def F(nums):
-  nums = sorted(list(set(nums)))
-  res = 1
-  result = 1
-  for i in range(1, len(nums)):
-    if nums[i] == nums[i-1] + 1:
-      res += 1
-      result = max(res, result)
-    else:
-      res = 1
-  return result
+
+# dfs: 1을 0으로 만들고 상하좌우 dfs.
+
+def F(grid):
+  def dfs(i, j):
+    if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j]=="0":
+      return
+    grid[i][j] = "0"
+    dfs(i+1, j)
+    dfs(i-1, j)
+    dfs(i, j-1)
+    dfs(i, j+1)
+  count = 0
+  for i in range(len(grid)):
+    for j in range(len(grid[0])):
+      if grid[i][j] == "1":
+        count += 1
+        dfs(i,j)
+  return count
+
+
+  
